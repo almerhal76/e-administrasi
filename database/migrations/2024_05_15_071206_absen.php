cbdc61cc1750ec935498eff496e3da68c680d634
herisetyoaji <heriaji72@gmail.com>
2024-05-15 15:01:14 +0700

    commit lupa ganti izin jadi absen haha

--- database/migrations/2024_05_15_071206_absen.php
+++ database/migrations/2024_05_15_071206_absen.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('izin', function (Blueprint $table) {
+        Schema::create('absen', function (Blueprint $table) {
             $table->id();
             $table->string('nama_karyawan'); //cara connectnya belum tau
             $table->datetime('tanggal_masuk')->nullable();
